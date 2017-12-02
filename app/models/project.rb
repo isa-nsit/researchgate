3fc664a43773849e9f3a70ecad7fe0ecd0f57351
AshishChauhan <ashishc534@gmail.com>
2017-12-02 12:00:34 +0530

    Front End Left

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ActiveRecord::Base
 	validates :name, :desc, :category, presence: true
 	belongs_to :admin
 	has_and_belongs_to_many :users
+	default_scope  { order(:created_at => :desc) }
 end
