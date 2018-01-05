9a037eb1935f5102a7a4d3dc3e9cef8361c81ba6
Ankush8527 <ankushsharma100997@gmail.com>
2018-01-06 02:47:01 +0530

    facultyShowPartiallyDone

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -80,7 +80,7 @@ class TasksController < ApplicationController
         end
         
         format.html { redirect_to active_user_home_path, notice: 'Response has been sent' }
-        #FormMailer.FormSubmission(@task,@email).deliver_now
+        FormMailer.FormSubmission(@task,@email).deliver_now
 
       else
         format.html { redirect_to tasks_new_path , notice: 'You have not filled all the fields or email id is not registered with us' }
