88e9159dd44975bed593da3d9689665ea095bacf
Ashish Chauhan <ashishc534@gmail.com>
2018-02-04 20:22:56 +0530

    Checking integer

--- db/migrate/20171208090857_create_project_user_admin_relations.rb
+++ db/migrate/20171208090857_create_project_user_admin_relations.rb
@@ -2,7 +2,7 @@ class CreateProjectUserAdminRelations < ActiveRecord::Migration
   def change
     create_table :project_user_admin_relations do |t|
       t.references :admin, index: true,type: :integer, foreign_key: true
-      t.references :user, index: true, type: :integer,type: :uuid,foreign_key: true
+      t.references :user, index: true,type: :integer, foreign_key: true
       t.references :SelectedProject, index: true,type: :integer, foreign_key: true
 
       t.timestamps null: false
