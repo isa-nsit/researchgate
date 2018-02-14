c027a4a6bffe50f8586828030d8bbc7640d08350
Ankush8527 <ankushsharma100997@gmail.com>
2018-02-15 04:10:48 +0530

    Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,11 +61,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-Rails.application.routes.default_url_options[:host] = 'myappsname.herokuapp.com'
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default :charset => "utf-8"
+config.action_mailer.default_url_options = { :host => 'isansit.heroku.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
