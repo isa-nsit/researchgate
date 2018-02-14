2dec37dda0eb3c4b3372ab1fd2450fa725c7f117
Ankush8527 <ankushsharma100997@gmail.com>
2018-02-15 04:31:47 +0530

    Mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+config.action_mailer.default_url_options = { :host => 'isansit.heroku.com' }
 
 # Initialize the Rails application.
 Rails.application.initialize!
