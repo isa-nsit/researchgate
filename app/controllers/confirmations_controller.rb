40b82a9d24514a0bbf6e9fa20ff5826549934a47
Ankush8527 <ankushsharma100997@gmail.com>
2018-02-17 03:47:18 +0530

    Mailer

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,12 @@
+class ConfirmationsController < Devise::ConfirmationsController
+
+private
+def after_confirmation_path_for(resouce_name,resource)
+
+    http://researchgate.isa-nsit.in/users/sign_in
+
+	end
+
+
+
+end
