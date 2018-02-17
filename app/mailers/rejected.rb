15aaf887c5ca3d3d8888f856522aea3b4c27e09f
Ankush8527 <ankushsharma100997@gmail.com>
2018-02-17 20:48:52 +0530

    Mailer

--- app/mailers/rejected.rb
+++ app/mailers/rejected.rb
@@ -3,7 +3,8 @@ class Rejected < ApplicationMailer
 	def FormSubmission(task,email)
        @task=task
        @email=email
-       @ProfName=Prof
+        
+    @admin=Admin.where(:id=>@task.admin_id.to_s).name
        mail(to: @email, subject: 'Your idea has been Rejected')
 
 	end
